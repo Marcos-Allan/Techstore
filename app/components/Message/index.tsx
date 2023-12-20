f82fbb5d7271c2fbe604d58113eaeb46bf37a142
Marcos Allan <allanmenezes880@gmail.com>
2023-12-20 14:44:32 -0300

    adição da autenticação com o google e o firebase

--- app/components/Message/index.tsx
+++ app/components/Message/index.tsx
@@ -1,18 +1,68 @@
+'use client'
+import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithRedirect, signInWithPopup, signOut } from "firebase/auth";
+import '@/app/utils/firebase.js'
+
 export default function Message() {
+
+    const auth = getAuth();
+
+    const provider = new GoogleAuthProvider();
+
+    function login(){
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            const cred = GoogleAuthProvider.credentialFromResult(result)
+            console.log(cred)
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                  // User is signed in, see docs for a list of available properties
+                  // https://firebase.google.com/docs/reference/js/auth.user
+                  const uid = user.uid;
+                  console.log(user.displayName)
+                  console.log(user.email)
+                  console.log(user.photoURL)
+                  // ...
+                } else {
+                  // User is signed out
+                  // ...
+                }
+              });
+        })
+      .catch((error) => {
+        console.log(error)
+      });
+    }
+
+    function logout(){
+        signOut(auth)
+        console.log('LOGOUT')
+    }
+
    return (
         <div
-            className="fixed bottom-4 bg-h-white-200 h-auto opacity-90 w-10/12 p-4 overflow-hidden rounded-[8px] pb-[60px] border-[1px] border-solid border-h-gray-300"
+            className="fixed bottom-4 bg-h-white-200 h-auto opacity-90 w-10/12 p-4 overflow-hidden rounded-[8px] pb-[100px] border-[1px] border-solid border-h-gray-300"
         >
             <p
                 className="text-black text-center"
             >Parece que você ainda não fez login, faça para aproveitar o maximo possivel dos dados</p>
             <div
-                className="absolute w-full bottom-0 left-0 bg-h-gray-300 flex items-center py-2 justify-center h-[46px]"
+                onClick={() => login()}
+                className="absolute w-full bottom-[50px] left-0 bg-h-gray-300 flex items-center py-2 justify-center h-[46px]"
             >
                 <p
                     className="text-center"
                 >
                     Fazer Login
+                </p>
+            </div>
+            <div
+                onClick={() => logout()}
+                className="absolute w-full bottom-0 left-0 bg-h-gray-300 flex items-center py-2 justify-center h-[46px]"
+            >
+                <p
+                    className="text-center"
+                >
+                    Fazer Logout
                 </p>
             </div>
         </div>
