0f9eacae063bd19a34a508af821f55753d1317fa
Marcos Allan <allanmenezes880@gmail.com>
2023-12-24 17:52:21 -0300

    trooca da interface do menu

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -2,6 +2,8 @@
 import { useState } from 'react'
 import { HiOutlineMenu } from "react-icons/hi";
 import { HiX } from "react-icons/hi";
+import { HiUserAdd } from "react-icons/hi";
+import { HiSearch } from "react-icons/hi";
 import ToggleDarkMode from './ToggleDarkMode';
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -13,19 +15,20 @@ export default function Menu() {
     
     const pathname = usePathname()
     const [open, setOpen] = useState<boolean>(false)
+    const [openSearch, setOpenSearch] = useState<boolean>(false)
 
     function isProductInfo(){
-        if(pathname == '/'){
-            return false
-        }else if(pathname == ('/product/656b56902a3681082eaae293') || ('/product/6574d8af7fa62604260bd7ab') || ('/product/65772915ecb548f507f05657') || ('/product/     65772adaf22a22bae3748983') || ('/product/65772b36f22a22bae374898b') || ('/product/65772b74f22a22bae374898f') || ('/product/65772cec250f13950c848697')){
-            return true
-        }
+        return true
     }
+
     return(
         <>
             <div
-                className={`w-full ${isProductInfo() === true ? 'h-[80px]' : 'h-[120px]'}
-                fixed top-0 left-0 z-[2] ${theme == 'light' ? 'bg-h-white-200' : 'bg-h-gray-300'} px-6 py-3 flex justify-around items-center flex-wrap`}
+                className={`
+                    ${isProductInfo() === true ? 'h-[80px]' : 'h-[120px]'}
+                    ${theme == 'light' ? 'bg-h-white-200' : 'bg-h-gray-300'}
+                    px-6 py-3 flex justify-around items-center flex-wrap w-full fixed top-0 left-0 z-[2]
+                `}
             >
                 <Link
                     href={'/'}
@@ -33,68 +36,110 @@ export default function Menu() {
                 >
                     <h1
                         className={`text-[19px] font-medium ${theme == 'light' ? 'text-black' : 'text-white'}`}
-                        >
+                    >
                         TechStore
                     </h1>
                 </Link>
 
-                <div className="flex-grow-[1] flex gap-2">
-                    <div className={`flex-1 flex justify-center py-1 items-center rounded-[8px] hover:opacity-50
-                    ${theme == 'light' ? 'bg-h-gray-300' : 'bg-h-white-200'}`
-                }>
+                <div className="flex-grow-[1] flex gap-4 justify-end items-center">
+                    <Link
+                        href={'/login'}
+                        className={`
+                            flex justify-center py-1 items-center flex-col rounded-[8px] hover:opacity-50
+                        `}
+                    >
+                        <HiUserAdd
+                            className={`
+                                    text-[22px] text-center
+                                    ${theme == 'light' ? 'text-h-gray-300' : 'text-h-white-200'}
+                                `}
+                            />
                         <p
-                            className={`text-[12px] text-center leading-[15px]
-                            ${theme == 'light' ? 'text-white' : 'text-black'}`}
-                        >Criar Conta</p>
-                    </div>
-                    
-                    <Link href={'/login'} className={`flex-1 flex justify-center py-1 items-center
-                    rounded-[8px] hover:opacity-50
-                    ${theme == 'light' ? 'bg-h-gray-300' : 'bg-h-white-200'}`
-                    }>
-                        <p
-                            className={`text-[12px] text-center
-                            ${theme == 'light' ? 'text-white' : 'text-black'}`}
-                        >Conecte-se</p>
+                            className={`
+                                text-[8px] text-center
+                                ${theme == 'light' ? 'text-h-gray-300' : 'text-h-white-200'}
+                            `}
+                            >Conecte-se</p>
                     </Link>
 
                     <div
+                        onClick={() => setOpenSearch(true)}
+                    >
+                        <HiSearch
+                            className={`
+                                text-[22px] text-center
+                                ${theme == 'light' ? 'text-h-gray-300' : 'text-h-white-200'}
+                            `}
+                        />
+                    <p
+                        className={`
+                            text-[8px] text-center
+                            ${theme == 'light' ? 'text-h-gray-300' : 'text-h-white-200'}
+                        `}
+                        >Pesquisar</p>
+                    </div>
+
+                    <div
                         onClick={() => setOpen(true)}
-                        className="flex justify-center items-center"
+                        className="flex justify-center items-center flex-col"
                     >
                         <HiOutlineMenu
-                            className={`text-[24px] ${theme == 'light' ? 'text-h-gray-300' : 'text-h-white-200'} hover:opacity-50`}
+                            className={`
+                                text-[22px] text-center
+                                ${theme == 'light' ? 'text-h-gray-300' : 'text-h-white-200'}
+                            `}
                         />
+                    <p
+                        className={`
+                            text-[8px] text-center
+                            ${theme == 'light' ? 'text-h-gray-300' : 'text-h-white-200'}
+                        `}
+                        >Menu</p>
                     </div>
                 </div>
 
-                {isProductInfo() === false && (
-                    <div className={`w-full h-[36px] flex mt-3`}>
-                        <input
-                            type="text"
-                            placeholder="pesquisar na techstore"
-                            className={`flex-grow-[1] ps-3 h-[36px] leading-5
+            </div>
+                <div className={`
+                    w-full h-[110%] fixed ${openSearch == true ? 'top-0' : '-top-[110%]'} left-0 z-50 opacity-95 flex justify-center items-start transition-all duration-500
+                    ${theme == 'light' ? 'bg-h-white-100' : 'bg-h-black-500'}
+                `}>
+                <div
+                    className={`
+                        w-10/12 flex flex-row rounded-[8px] overflow-hidden mt-[100px]
+                    `}
+                >
+                    <input 
+                        type='text'
+                        placeholder='pesquisar na techstore'
+                        className={`
+                            flex-grow-[1] h-[30px] ps-5 outline-none
+                            ${theme == 'light' ? 'bg-h-white-200' : 'bg-h-gray-300'}
                             ${theme == 'light' ? 'text-black' : 'text-white'}
-                            ${theme == 'light' ? 'bg-h-white-100' : 'bg-h-black-500'}
-                            capitalize rounded-tl-[20px] text-[12px] flex items-center rounded-bl-[36px] placeholder:${theme === 'light' ? 'text-h-gray-300' : 'text-h-white-200'} focus:outline-none`}
+                            border border-solid ${theme == 'light' ? 'border-black' : 'border-white'}
+                            rounded-tl-[8px] rounded-bl-[8px]
+                            placeholder:${theme == 'light' ? 'text-black' : 'text-white'} placeholder:opacity-80 capitalize
+                        `}
+                    />
+                    <button
+                        className={`w-[30px] border border-solid ${theme == 'light' ? 'border-black' : 'border-white'} h-[30px] flex rounded-tr-[8px] rounded-br-[8px] items-center justify-center ${theme == 'light' ? 'bg-h-white-200' : 'bg-h-gray-300'}`}
+
+                    >
+                        <HiX
+                            onClick={() => setOpenSearch(false)}
+                            className={`
+                            ${theme == 'light' ? 'text-black' : 'text-white'}
+                            text-[18px]
+                            `}
                         />
-                        <button
-                            className={`text-[12px] px-2
-                            ${theme == 'light' ? 'text-white' : 'text-black'}
-                            ${theme == 'light' ? 'bg-h-gray-300' : 'bg-h-white-200'}
-                            rounded-tr-[20px] rounded-br-[20px] hover:opacity-50`}
-                        >
-                            Pesquisar
-                        </button>
-                    </div>
-                )}
+                    </button>
+                </div>
             </div>
 
             {/* MENU LATERAL MOBILE */}
             <div
                 className={`
                 ${theme == 'light' ? 'bg-h-white-100' : 'bg-h-black-500'}
-                w-full h-[110%]
+                w-full h-[110%] duration-500
                 block fixed top-0 ${open == true ? '-left-[0%]' : '-left-[100%]'} z-[4] p-5 transition-all
                 `}
             >
--- app/components/Screen.tsx
+++ app/components/Screen.tsx
@@ -9,7 +9,7 @@ export default function Screen({children}:{children:React.ReactNode}){
     return(
         <div className={`
             ${theme == 'light' ? 'bg-h-white-100' : 'bg-h-black-500'}
-            w-full min-h-screen overflow-x-hidden flex flex-col flex-wrap justify-center wrap items-center pt-[136px]`}
+            w-full min-h-screen overflow-x-hidden flex flex-col flex-wrap justify-center wrap items-center pt-[80px]`}
         >
             {children}
         </div>
