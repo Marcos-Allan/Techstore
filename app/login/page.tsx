e8dcba29df6191fffc7e084150fd21b0bb149e70
Marcos Allan <allanmenezes880@gmail.com>
2023-12-26 21:33:48 -0300

    coloquei o inicio do login social na pagina login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,44 @@ import Image from 'next/image'
 import googleIcon from '@/public/Google_icon.png'
 import { useMyContext } from '@/providers/theme'
 
+import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithRedirect, signInWithPopup, signOut } from "firebase/auth";
+import '@/app/utils/firebase.js'
+
 export default function Login() {
+
+    const auth = getAuth();
+
+    const provider = new GoogleAuthProvider();
+
+    function login(){
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            const cred = GoogleAuthProvider.credentialFromResult(result)
+            console.log(cred)
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                  // User is signed in, see docs for a list of available properties
+                  // https://firebase.google.com/docs/reference/js/auth.user
+                  const uid = user.uid;
+                  console.log(user.displayName)
+                  console.log(user.email)
+                  console.log(user.photoURL)
+                  // ...
+                } else {
+                  // User is signed out
+                  // ...
+                }
+              });
+        })
+      .catch((error) => {
+        console.log(error)
+      });
+    }
+
+    function logout(){
+        signOut(auth)
+        console.log('LOGOUT')
+    }
 
     const states:any = useMyContext()
     const { theme, toggleTheme} = states
@@ -16,6 +53,7 @@ export default function Login() {
                 className={`${theme == 'light' ? 'bg-h-white-200' : 'bg-h-gray-300'} w-[80%] p-6 rounded-[8px]`}
             >
                 <div
+                    onClick={() => login()}
                     className={`w-full p-4 ${theme == 'light' ? 'bg-h-white-100' : 'bg-h-black-500'} flex rounded-[8px] items-center justify-around`}
                 >
                     <p
