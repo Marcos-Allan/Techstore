d848111ccc5b92dcf5160750e2c1d3492714abd3
Marcos Allan <allanmenezes880@gmail.com>
2023-12-27 15:41:11 -0300

    deletei arquivos inuteis

--- app/api/user/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server"
-
-export async function GET(req: Request) {
-    try {
-        return NextResponse.json( { message: 'SUCESSO' }, { status: 200} )
-    } catch (error) {
-        return NextResponse.json( { message: error }, { status: 405} )
-    }
-}
